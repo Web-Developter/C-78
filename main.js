29a0df58ff69f3d0768ce415abe5c7e1826c7608
Web-Developter <84792687+Web-Developter@users.noreply.github.com>
2021-09-25 11:53:47 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var images = ["family.jpg","grandpa.jpg", "father.jpg" , "bro.jpg", "sister.jpg", "mother.jpg"];
+var names = ["Family Book","Ranbir Singh", "Diljeet Singh", "Rocky Singh", "Alia Singh", "Soni Singh"];
+var i = 0;
+function update()
+{
+    i++;
+    var numbers_of_family_member_in_array = 5
+    if(i > numbers_of_family_member_in_array)
+      {
+          i = 0;
+      }
+    
+    var updatedImage = images[i];
+    var updatedName = names[i] ;
+ 
+    document.getElementById("family_member_image").src = updatedImage;
+    document.getElementById("family_member_name").innerHTML = updatedName;
+}
